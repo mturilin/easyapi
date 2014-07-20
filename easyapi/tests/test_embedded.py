941450a4647f82498b2a1de2c3897a3ce5248f28
Mikhail Turilin <mturilin@gmail.com>
2014-07-20 14:50:16 -0700

    bug fix introduced in the prev commit

--- easyapi/tests/test_embedded.py
+++ easyapi/tests/test_embedded.py
@@ -1,6 +1,10 @@
 import json
+
 import pytest
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_200_OK
+
 from easyapi.tests.factories import CompanyFactory, ProjectFactory
+
 
 __author__ = 'mikhailturilin'
 
@@ -11,12 +15,21 @@ def test_embedded(staff_api_client):
 
     projects = [ProjectFactory(company=company) for i in range(3)]
 
-
-    response = staff_api_client.get('/api/project/%d/' % projects[0].id, data={'_embedded':'company'})
-    assert response.status_code == 200
+    response = staff_api_client.get('/api/project/%d/' % projects[0].id, data={'_embedded': 'company'})
+    assert response.status_code == HTTP_200_OK
 
     response_data = json.loads(response.content)
 
     assert '_embedded' in response_data
     assert 'company' in response_data['_embedded']
     assert response_data['_embedded']['company']['id'] == company.id
+
+
+@pytest.mark.django_db
+def test_embedded_fail(staff_api_client):
+    company = CompanyFactory()
+
+    projects = [ProjectFactory(company=company) for i in range(3)]
+
+    response = staff_api_client.get('/api/project/%d/' % projects[0].id, data={'_embedded': 'id'})
+    assert response.status_code == HTTP_400_BAD_REQUEST
